1b99f1d8b8de4d7f84f72e90a4924a41649d55bb
phenom6oo <jonasalab@gmail.com>
2022-12-30 14:48:08 -0300

    Initial commit

---
+++ src/test/java/com/cursojava/cursoJona/CursoJonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursojava.cursoJona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
